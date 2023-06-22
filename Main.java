6dabcf7fcb35c155791154f074bff6436157d626
ParkDalJae <dv.over931@gmail.com>
2023-06-22 14:28:00 +0900

    연습중

---
+++ Main.java
@@ -0,0 +1,19 @@
+public class Main{
+  public static void main(String[] args){
+    //코드 작성 위치
+    int num = 10;
+    System.out.println(num);
+    main2(args);
+  }
+  public static void main2(String[] args){
+    int num = 7;
+    if(num>=10){
+      System.out.println("10보다 작습니다");
+    } else {
+      System.out.println("10보다 크거나 같습니다");
+    }
+  }
+  public static void main3(String[] args){
+    
+  }
+}
---
+++ Prac2.java
@@ -0,0 +1,19 @@
+public class Prac2 {
+  public static void main(String[] args) {
+    int num1 = 1;
+    int num2 = 5;
+    int add = num1 + num2;
+    int min = num1 - num2;
+    int div = num1 / num2;
+    int mul = num1 * num2;
+    int rem = num1 % num2;
+    System.out.println("num 1 = " + num1);
+    System.out.println("num 2 = " + num2);
+    System.out.println("num 1 + num 2 = " + add);
+    System.out.println("num 1 - num 2 = " + min);
+    System.out.println("num 1 / num 2 = " + div);
+    System.out.println("num 1 * num 2 = " + mul);
+    System.out.println("num 1 % num 2 = " + rem);
+    
+  }
+}
